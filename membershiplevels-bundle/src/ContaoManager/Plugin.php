92753e13933791024b35a5776d2b08d8fef86bb5
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-13 13:01:45 +0200

    Update Plugin.php

--- membershiplevels-bundle/src/ContaoManager/Plugin.php
+++ membershiplevels-bundle/src/ContaoManager/Plugin.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Localbranding-de\Membershiplevels\ContaoManager;
+namespace Localbranding-de\MembershiplevelsBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
