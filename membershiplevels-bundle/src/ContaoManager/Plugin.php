035e4d225551bce99bb628ab7a860449fddce663
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-13 13:02:13 +0200

    Update Plugin.php

--- membershiplevels-bundle/src/ContaoManager/Plugin.php
+++ membershiplevels-bundle/src/ContaoManager/Plugin.php
@@ -14,7 +14,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Localbranding-de\Membershiplevels\Membershiplevels;
+use Localbranding-de\MembershiplevelsBundle\Membershiplevels;
 
 class Plugin implements BundlePluginInterface
 {
