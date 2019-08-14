116f443688cd54b26c019c9a9106e50792ff349c
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-14 13:19:25 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,15 +1,8 @@
 <?php
-/*
- * Cookiebar extension for Contao Open Source CMS
- *
- * Copyright (C) 2011-2018 Codefog
- *
- * @author  Codefog <https://codefog.pl>
- * @author  Kamil Kuzminski <https://github.com/qzminski>
- * @license MIT
- */
 
 /**
  * Hooks.
  */
 $GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('LocalbrandingDe\MembershiplevelsBundle\honischClass', 'myOutputBackendTemplate');
+// Frontend modules
+$GLOBALS['FE_MOD']['miscellaneous']['helloWorld'] = 'LocalbrandingDe\MembershiplevelsBundle\Classes\HelloWorldModule';
