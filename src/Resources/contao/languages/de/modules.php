ba55e4d08d4c8007cabf8413e44d81aa075a8f18
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-14 13:18:31 +0200

    Add files via upload

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -3,3 +3,5 @@ $GLOBALS['TL_LANG']['MOD']['member']['0']                    = 'Member';
 $GLOBALS['TL_LANG']['MOD']['member']['1']                    = 'Member verwalten';
 $GLOBALS['TL_LANG']['MOD']['mgroup']['0']                    = 'Membergruppen';
 $GLOBALS['TL_LANG']['MOD']['mgroup']['1']                    = 'Membergruppen verwalten';
+// Frontend modules
+$GLOBALS['TL_LANG']['FMD']['helloWorld'] = ['Hello World', 'Gibt den Text "Hello World" aus'];
