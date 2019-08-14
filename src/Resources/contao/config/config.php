ea52bbc42c59bd3f3b5b59c45f5f40e70d970cef
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-14 12:05:51 +0200

    Add files via upload

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,16 @@
+<?php
+namespace Memberlevels;
+/*
+ * Cookiebar extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2011-2018 Codefog
+ *
+ * @author  Codefog <https://codefog.pl>
+ * @author  Kamil Kuzminski <https://github.com/qzminski>
+ * @license MIT
+ */
+
+/**
+ * Hooks.
+ */
+$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('\Memberlist\honischClass::class', 'myOutputBackendTemplate');
---
+++ src/Resources/contao/languages/de/tl_member_group.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_member_group']['memberpoints'] = array('Benötigte Memberpunkte', 'Bitte geben Sie an wie viele Memberpunkte diese Gruppe benötigt');
+$GLOBALS['TL_LANG']['tl_member_group']['isSpecial'] = array('Ist Sondergruppe', 'Wählen Sie hier wenn diese Gruppe nur durch TSC-Mitarbeiter vergeben werden kann. Die Gruppe wird dann aus der Aufstiegslogik ausgeschlossen');
