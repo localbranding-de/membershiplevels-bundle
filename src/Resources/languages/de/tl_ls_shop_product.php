4f1b5e03a172395dbd89c65036d375ee647da60f
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-14 11:22:04 +0200

    Add files via upload

---
+++ src/Resources/languages/de/tl_ls_shop_product.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_ls_shop_product']['lb_memberpointvalue'] = array('Memberpunkte' , 'Geben Sie hier ein wie viele Memberpunkte dem Kunden beim Kauf gutgeschrieben werden');
+$GLOBALS['TL_LANG']['tl_ls_shop_product']['lsShopMemberlevel'] = 'Memberpunkt-Wert';
+$GLOBALS['TL_LANG']['tl_ls_shop_product']['productgroups'] = array('Für Membergruppe(n)','Wählen Sie die Membergruppen aus welche das produkt kaufen dürfen');
---
+++ src/Resources/languages/de/tl_member.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_member']['memberpoints'] = array('Memberpunkte', 'Aktuelle Memberpunkte');
+
