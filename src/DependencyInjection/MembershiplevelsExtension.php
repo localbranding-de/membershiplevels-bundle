228c634ab96aedf106e2d6a1dd0b091207857dd8
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-14 11:00:10 +0200

    Update MembershiplevelsExtension.php

--- src/DependencyInjection/MembershiplevelsExtension.php
+++ src/DependencyInjection/MembershiplevelsExtension.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Localbranding-de\MembershiplevelsBundle\DependencyInjection;
+namespace LocalbrandingDe\MembershiplevelsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
