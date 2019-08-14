82ab65963e9470e78718d38fdfbf7f0557a023a2
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-14 12:05:10 +0200

    Delete modules.php

--- src/Resources/languages/de/modules.php
+++
@@ -1,5 +0,0 @@
-<?php
-$GLOBALS['TL_LANG']['MOD']['member']['0']                    = 'Member';
-$GLOBALS['TL_LANG']['MOD']['member']['1']                    = 'Member verwalten';
-$GLOBALS['TL_LANG']['MOD']['mgroup']['0']                    = 'Membergruppen';
-$GLOBALS['TL_LANG']['MOD']['mgroup']['1']                    = 'Membergruppen verwalten';
