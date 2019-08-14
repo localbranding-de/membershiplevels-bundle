d2351edb28a873b64502c4865f6f5d841eb0a5b8
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-14 10:59:26 +0200

    Update MembershiplevelsBundle.php

--- src/MembershiplevelsBundle.php
+++ src/MembershiplevelsBundle.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Localbranding-de\MembershiplevelsBundle;
+namespace LocalbrandingDe\MembershiplevelsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
