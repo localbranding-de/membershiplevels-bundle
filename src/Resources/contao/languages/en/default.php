0e8f47d89cd0fc07481aa0c8734c2895f9bc77bc
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-15 14:58:12 +0200

    Add files via upload

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Frontend ->
+ */
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText005'] = 'Place in shopping bag';
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText008'] = 'Check your shopping  bag.';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText008-01'] = 'Your shopping bag.';
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText025'] = 'The shopping bag does not contain any items.';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText038'] = 'Not all products of your order are in stock in the desired quantity. The quantities in the shopping bag have been adapted accordingly.';
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText043'] = 'The shopping bag is empty.';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText044'] = '%s product in shopping bag';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText045'] = '%s products in shopping bag';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText046'] = 'Go to shopping bag';
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText056'] = 'This product is no longer in stock in the desired quantity of %s %s. Therefore, the ordered quantity in the shopping bag has been reduced to the available quantity of %s %s.';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText057'] = 'This product is no longer in stock in the desired quantity of %s %s. Therefore, the ordered quantity in the shopping bag has been set to the quantity of %s %s which is still available.';
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText131'] = 'Shopping Bag';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText132'] = 'Open shopping bag';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText133'] = 'The shopping bag doesn\'t contain any products yet.';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['miscText134'] = 'Products in the shopping bag';
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['misc']['hasBeenPutInCart'] = '%s of this/these product(s) has/have been added to the shopping bag!';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['misc']['couldNotBePutInCart'] = 'The product could not be added to the shopping bag.';
+
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['checkoutFlow']['cart'] = 'Shopping bag';
+
+ /*
+  * <- Backend
+  */
+ ?>
