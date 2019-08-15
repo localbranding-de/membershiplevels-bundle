c097b5899b812c2e6a418d9ea11063461b6ab128
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-15 14:54:40 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,6 +3,6 @@
 /**
  * Hooks.
  */
-$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('LocalbrandingDe\MembershiplevelsBundle\EventListener\OutputBackendTemplateListener::class', 'myOutputBackendTemplate');
+$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array(\LocalbrandingDe\MembershiplevelsBundle\EventListener\OutputBackendTemplateListener::class, 'myOutputBackendTemplate');
 // Frontend modules
 $GLOBALS['FE_MOD']['miscellaneous']['helloWorld'] = 'LocalbrandingDe\MembershiplevelsBundle\HelloWorldModule';
