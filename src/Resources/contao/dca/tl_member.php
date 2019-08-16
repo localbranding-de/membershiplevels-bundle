eb28c02011fdba19038b2c49b3bd82cdf6e41b1c
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-16 14:43:30 +0200

    Add files via upload

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -5,7 +5,7 @@
 
 
 //Legenden hinzuf�gen
-$GLOBALS['TL_DCA']['tl_member']['palettes']['default']=str_replace('lastname','lastname,memberpoints,membergroup',$GLOBALS['TL_DCA']['tl_member']['palettes']['default'] );
+$GLOBALS['TL_DCA']['tl_member']['palettes']['default']=str_replace('lastname','lastname,memberNr,memberpoints,membergroup',$GLOBALS['TL_DCA']['tl_member']['palettes']['default'] );
 
 // Hinzuf�gen der Feld-Konfiguration
 $GLOBALS['TL_DCA']['tl_member']['fields']['memberpoints'] = array
@@ -24,7 +24,6 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['membergroup'] = array
     'exclude'   => true,
     'inputType' => 'text',
     'load_callback'    => array('classMember', 'myLoadCallback'),
-    'input_field_callback'    => array('classMember', 'myInputFieldCallback'),
     'foreignKey'              => 'tl_member_group.name',
     'eval'      => array('disabled'=>true),
     'sql'       => "blob NULL"
@@ -40,6 +39,15 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['groups'] = array
     'eval'                    => array( 'feEditable'=>true, 'feGroup'=>'login'),
     'sql'                     => "blob NULL",
     'relation'                => array( 'load'=>'lazy')
+);
+// Hinzuf�gen der Feld-Konfiguration
+$GLOBALS['TL_DCA']['tl_member']['fields']['memberNr'] = array
+(
+    'label'     => &$GLOBALS['TL_LANG']['tl_member']['memberNr'],
+    'inputType' => 'text',
+    'eval' => array('unique'=>true, 'rgxp'=>'natural','disabled'=>true),
+    'filter'                  => true,
+    'sql'                     => "varchar(11) NULL",
 );
 
 class classMember extends Backend
