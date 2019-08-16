a159d3b718ffcb062f2339a310414dfe5f936637
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-16 14:44:25 +0200

    Add files via upload

--- src/Resources/contao/languages/de/tl_member.php
+++ src/Resources/contao/languages/de/tl_member.php
@@ -1,4 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_member']['memberpoints'] = array('Memberpunkte', 'Aktuelle Memberpunkte');
-
+$GLOBALS['TL_LANG']['tl_member']['memberNr'] = array('Membernummer', 'Individuelle Membernummer');
