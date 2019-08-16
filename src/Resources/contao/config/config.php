b89f7caa44e78cb987b5c94d4c2ea0d418591f4f
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-16 14:44:40 +0200

    Add files via upload

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,5 +4,8 @@
  * Hooks.
  */
 $GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array(\LocalbrandingDe\MembershiplevelsBundle\EventListener\OutputBackendTemplateListener::class, 'myOutputBackendTemplate');
+$GLOBALS['TL_HOOKS']['parseTemplate'][] = array(\LocalbrandingDe\MembershiplevelsBundle\EventListener\FrontendTemplateListener::class, 'myParseFrontendTemplate');
+$GLOBALS['TL_HOOKS']['activateAccount'][] = array(\LocalbrandingDe\MembershiplevelsBundle\EventListener\activateAccountListener::class, 'myActivateAccount');
 // Frontend modules
 $GLOBALS['FE_MOD']['miscellaneous']['helloWorld'] = 'LocalbrandingDe\MembershiplevelsBundle\HelloWorldModule';
+//$GLOBALS['TL_HOOKS']['createNewUser'][] = array(\LocalbrandingDe\MembershiplevelsBundle\EventListener\createNewUserListener::class, 'myCreateNewUser');
